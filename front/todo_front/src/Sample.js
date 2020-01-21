fec1982ea2b2daa60bfe38648eea9f03c9765c46
eRy-sk <ride.poke@gmail.com>
2020-01-21 23:19:57 +0900

    Add Sample component

---
+++ front/todo_front/src/Sample.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const foo = "foo";
+const bar = "bar";
+
+function Sample() {
+    return (
+        <div className="App">
+            <h1>{foo}</h1>
+            <h2>{bar}</h2>
+        </div>
+    );
+}
+
+export default Sample;
