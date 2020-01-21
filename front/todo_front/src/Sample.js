ce1e336c7376e9ca93e21096081dff8a4242defd
eRy-sk <ride.poke@gmail.com>
2020-01-21 23:31:15 +0900

    Fix style

--- front/todo_front/src/Sample.js
+++ front/todo_front/src/Sample.js
@@ -2,10 +2,14 @@ import React from 'react';
 
 const foo = "foo";
 const bar = "bar";
+const myStyle = {
+  fontSize: "20pt",
+  color: "blue"
+};
 
 function Sample() {
     return (
-        <div className="App">
+        <div style={myStyle}>
             <h1>{foo}</h1>
             <h2>{bar}</h2>
         </div>
